6775368e5045fe1f6e315ac9a2212702d0977e07
shiva <kumarrocks33@gamil.com>
2017-09-05 16:34:14 +0200

    2nd commit

---
+++ GitProject/src/com/leargit/Calculator.java
@@ -0,0 +1,15 @@
+package com.leargit;
+
+public class Calculator 
+{
+
+     public	int add (int a,int b)
+		{
+			int c = a+b;
+			System.out.println(c);
+			return c;
+		}
+                       
+}
+
+
